2eaf86a94d74f036803d23db2a77585d3e452ad6
Mohamed Shawky <mam2210_@hotmail.com>
2024-07-14 23:56:32 +0300

    Initial project setup

---
+++ src/main/java/com/deben/challenge/DebenCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.deben.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebenCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebenCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deben/challenge/DebenCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deben.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebenCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
